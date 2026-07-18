82423d7347b0f5a165e1a09e0e1d13caaa247403
wanga <wangalex8845@gmail.com>
2026-07-18 14:59:23 -0400

    made changes to linear x velocity

--- g1_fw_flat_rendering.py
+++ g1_fw_flat_rendering.py
@@ -32,10 +32,10 @@ def main():
     
     raw_env = registry.load(env_name, config_overrides={
         "impl": "jax",
-        "lin_vel_x": [0.5, 0.5],       
+        "lin_vel_x": [0.7, 0.7],       
         "lin_vel_y": [0.0, 0.0],       
         "ang_vel_yaw": [0.0, 0.0],    
-        #"command_config.b": [1.0, 1.0, 1.0]
+        "command_config.b": [1.0, 1.0, 1.0]
     })
     
     env = wrapper.wrap_for_brax_training(raw_env)
